29ddc13e6fca13d479f239a9a6ec9392ee9c15a6
Jaiboon Limpkittisin <jaiboon2005@gmail.com>
2024-05-29 10:26:55 +0700

    Edit Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,11 +1,11 @@
 num = int(input("Enter first value \n"))
 num2 = int(input("Enter second value \n "))
 method = str(input("Enter the procedure you want to calculate \n"))
-
+subtract = ["-", "subtract", "minus"]
 if "+" in method or "plus" in method:
     result = num + num2
     print(result)
-elif "-" in method or "subtract" in method or "minus" in method:
+elif method in subtract:
     result = num - num2
     print(result)
 elif "*" in method or "multiply" in method:
